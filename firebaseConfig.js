783100cd4417505fe0af5f04f7b214c011c873e2
jalamie <121021507+jalamie@users.noreply.github.com>
2025-04-17 09:28:07 +0800

    Update app
    removed firebaseConfig

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,7 @@
 // src/config/firebaseConfig.js or just firebaseConfig.js
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDc3qxRx6i28a_tY3bMB0tXWK3jM7MUo-g",
-    authDomain: "capstone-sal.firebaseapp.com",
-    projectId: "capstone-sal",
-    storageBucket: "capstone-sal.firebasestorage.app",
-    messagingSenderId: "506687469413",
-    appId: "1:506687469413:web:661076c64cf63e6197138c",
-    measurementId: "G-YPD0ZE5W55"
+    //place firebaseConfig here
   };
   
   export default firebaseConfig;
